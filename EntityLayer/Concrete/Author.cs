f999592f0d35b79410c89912adc40ce2cc24b2aa
Hasan Çağlar <hsncglr07@gmail.com>
2021-12-31 15:32:02 +0300

    .
    first commit (22)

---
+++ EntityLayer/Concrete/Author.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Author
+    {
+        [Key]
+        public int AuthorId { get; set; }
+
+        [StringLength(50)]
+        public string AuthorFullName { get; set; }
+
+        [StringLength(150)]
+        public string AuthorImage { get; set; }
+
+        [StringLength(300)]
+        public string AuthorAbout { get; set; }
+
+        //Yazar-Blog İlişkisi
+        public ICollection<Blog> Blogs { get; set; }
+    }
+}
